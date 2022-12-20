609c334356999e04ac92ea7fc54024ca132160b9
Vladimir Sadovnikov <sadko4u@gmail.com>
2022-12-21 01:24:21 +0300

    Version up

--- project.mk
+++ project.mk
@@ -24,7 +24,7 @@ ARTIFACT_NAME               = lsp-plugins-noise-generator
 ARTIFACT_TYPE               = plug
 ARTIFACT_DESC               = LSP Noise Generator Plugin Series
 ARTIFACT_HEADERS            = lsp-plug.in
-ARTIFACT_VERSION            = 1.0.0
+ARTIFACT_VERSION            = 1.0.1-devel
 
 
 
